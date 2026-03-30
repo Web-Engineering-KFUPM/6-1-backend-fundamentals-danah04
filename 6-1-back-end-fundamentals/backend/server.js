2ef1706f77a87f2ae0aeb635581f9a0eec06e363
danah04 <danahaldosary1@gmail.com>
2026-03-30 23:21:13 +0300

    completed TODO3

--- 6-1-back-end-fundamentals/backend/server.js
+++ 6-1-back-end-fundamentals/backend/server.js
@@ -6,8 +6,8 @@ import express from 'express';
 const app = express();
 
 // TODO 3: Allow React to access the server
-
-
+import cors from 'cors';
+app.use(cors());
 
 // TODO 5: Create the home route "/"
 
