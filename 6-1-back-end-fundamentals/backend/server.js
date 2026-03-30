28d865e0edd13708001a010bf74bf6f07663946a
danah04 <danahaldosary1@gmail.com>
2026-03-30 23:24:15 +0300

    completed TODO6

--- 6-1-back-end-fundamentals/backend/server.js
+++ 6-1-back-end-fundamentals/backend/server.js
@@ -16,7 +16,9 @@ app.get('/', (req, res) => {
 
 
 // TODO 6: Create the "/about" route
-
+app.get('/about', (req, res) => {
+    res.send('This is the about page!');
+});
 
 
 // TODO 7: Create the "/student" route
